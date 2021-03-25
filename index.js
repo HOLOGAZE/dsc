593a1ac838155f18f8e6da95f3b4626d8903df69
Noah <78118168+xxneokore@users.noreply.github.com>
2021-03-25 10:50:12 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const discord = require('discord.js');
+const client = new discord.Client;
+
+client.on('message', message => {
+
+prefix = 'j!'
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+
+    if (command == 'triggered') {
+        message.channel.send(`Don't pull a Goose...`)
+    } else if(command == 'guilded') {
+        message.channel.send(`https://www.guilded.gg/i/aEA97wwE`)
+    } else if(command == 'discord') {
+        message.channel.send(`https://discord.gg/7JtNvTtfuJ`)
+    } else if(command == 'noham') {
+        message.channel.send(`Shh.. That never happened.`)
+    } else if(command == 'juice') {
+        message.channel.send(`hey`)
+    } else if(command == 'kiss') {
+        message.channel.send(`back off...`)
+    } else if(command == 'say') {
+        const say = args.join(" ");
+        message.channel.send(say)
+        message.delete()
+    }
+});
+
+client.login('NzU1ODU0ODk1NjQyNDQzNzc3.X2JWug.ILzfs9C26ZxAO-8CZzW8lgLParc');
